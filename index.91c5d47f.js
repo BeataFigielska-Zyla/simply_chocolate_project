5128312ec95e718ca1d6a953200c44db6bab4b97
BeataFigielska-Zyla <144831693+BeataFigielska-Zyla@users.noreply.github.com>
2023-11-21 00:00:13 +0000

    Deploying to gh-pages from @ BeataFigielska-Zyla/simply_chocolate_project@276bbe3efc8ece8601a5d09a8a6523942f9e8f44 🚀

--- index.91c5d47f.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".slide-content",{slidesPerView:4,spaceBetween:25,loop:!0,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!1},breakpoints:{0:{slidesPerView:1},769:{slidesPerView:2},1201:{slidesPerView:4}}});
-//# sourceMappingURL=index.91c5d47f.js.map
