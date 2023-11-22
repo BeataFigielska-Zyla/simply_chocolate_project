29447461007cda1620ab1112ae051e9b191b55a0
BeataFigielska-Zyla <144831693+BeataFigielska-Zyla@users.noreply.github.com>
2023-11-22 21:01:44 +0000

    Deploying to gh-pages from @ BeataFigielska-Zyla/simply_chocolate_project@5aeaa11f312ec0d676ad04d50c20e21f4f6a3ec9 🚀

--- index.69367c50.js
+++ index.69367c50.js
@@ -1,2 +1,2 @@
 (()=>{const e={openModalBtn:document.querySelector("[data-modal-open-buy]"),closeModalBtn:document.querySelector("[data-modal-close-buy]"),modal:document.querySelector("[data-modal-buy]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.34d46a91.js.map
+//# sourceMappingURL=index.69367c50.js.map
